89eb3971f9d5809908aea2d105eb4bfa6065f930
Juho Hovila <juho.hovila@arm.com>
2020-11-02 10:56:09 +0200

    Disable usage of jlink probes

--- setup.py
+++ setup.py
@@ -113,7 +113,8 @@ setup(
         ],
         'pyocd.probe': [
             'cmsisdap = pyocd.probe.cmsis_dap_probe:CMSISDAPProbePlugin',
-            'jlink = pyocd.probe.jlink_probe:JLinkProbePlugin',
+# https://armiot.atlassian.net/browse/ISGTESTINF-10685
+#            'jlink = pyocd.probe.jlink_probe:JLinkProbePlugin',
             'remote = pyocd.probe.tcp_client_probe:TCPClientProbePlugin',
             'stlink = pyocd.probe.stlink_probe:StlinkProbePlugin',
         ],
